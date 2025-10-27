c23cc053519a542e6a6065b97106173815d33839
Jennifer Egwu <jennifer.egwu@reventtechnologies>
2025-10-27 04:39:52 +0100

    Changes to entryfile.sh, nginx.conf.template

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-envsubst '$PORT $APP_POOL $RELEASE_ID' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$PORT' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
-
-nginx -t
-
-nginx -s reload 
 
 exec nginx -g 'daemon off;'
